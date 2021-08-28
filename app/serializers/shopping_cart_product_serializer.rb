dc5988e180ed0d81623bf55b45ca20a4619221e5
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-28 13:34:38 +0800

    Update cart attributes

--- app/serializers/shopping_cart_product_serializer.rb
+++ app/serializers/shopping_cart_product_serializer.rb
@@ -4,7 +4,15 @@ class ShoppingCartProductSerializer
 
     attributes :product_id, :quantity
 
+    attribute :price do |scp|
+        scp.product.price
+    end
+
     attributes :total_price do |scp| 
         scp.total_price
+    end
+
+    attributes :image do |scp| 
+        scp.product.image
     end
 end
