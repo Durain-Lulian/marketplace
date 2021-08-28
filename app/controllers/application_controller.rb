f07c9c1bba8e888721a0f565f23127e3f9dc8b9a
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-28 10:29:54 +0800

    Add healthcheck

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def healthcheck
+        render json: { message: 'Marketplace Service is healthy' }, status: 200
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # healthcheck
+  get '/marketplace/healthcheck', to: 'application#healthcheck'
+
   # shops
   get '/shops-by-category', to: 'shop#get_shops_by_category'
 
